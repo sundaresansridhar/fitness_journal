58833579899241e4700198653d511a0472d9fcab
Sundaresan Sridhar <fusion.frontiers.info@gmail.com>
2024-09-07 08:13:00 +0100

    journal merging details are updated

--- generate_journal.py
+++ generate_journal.py
@@ -7,91 +7,74 @@ Created on Thu Aug 29 18:55:51 2024
 
 @author: sundaresan_sridhar
 """
-from PyPDF2 import PdfMerger, PdfReader, PdfWriter
+
 import os
-from generate_daily_entries import types,version, year
-from io import BytesIO
+from generate_daily_entries import foldernames, input_dict
+from generate_intro import get_intro_merging_details
+from generate_monthly_entries import get_monthly_merging_details
 
-""" current directory """
+
+""" current directory"""
 current_directory = os.path.abspath(os.path.dirname(__file__))
- 
-def merge_pdfs(filepath_list, output_path, page_to_merge = None):
-    """
-    Merges multiple PDF files into a single PDF.
 
-    Parameters:
-        filepath_list (list): List of paths to the PDF files to be merged.
-        output_path (str): Path where the merged PDF will be saved.
-    """
-    merger = PdfMerger()
-    if page_to_merge is None:
-        page_to_merge = [None for i in filepath_list]
-    try:
-        # Iterate over the list of PDF files
-        for ipdf,pdf in enumerate(filepath_list):
-            if page_to_merge[ipdf] is None:
-                # Append each PDF file to the merger object
-                merger.append(pdf)
-            else:
-                
-                # Read the PDF file
-                reader = PdfReader(pdf)
-                # Extract the selected page
-                selected_page = reader.pages[page_to_merge[ipdf]]
-                
-                # Create a PdfWriter object to write the first page to a new PDF
-                writer = PdfWriter()
-                writer.add_page(selected_page)
-    
-                # Write the single page PDF to a BytesIO object (in-memory buffer)
-                temp_pdf = BytesIO()
-                writer.write(temp_pdf)
-                temp_pdf.seek(0)
-    
-                # Append the in-memory PDF (first page) to the merger
-                merger.append(temp_pdf)
-            print(f"PDFs {pdf.split('/')[-1]} merged successfully into {output_path}")
-                
-        # Write the merged PDF to the specified output path
-        merger.write(output_path)
-        
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    finally:
-        # Close the merger object
-        merger.close()
-    
 
-if __name__=='__main__':
-    ''' getting all the filepaths'''
-    intro_filepaths = [
-        [os.path.join(current_directory, "all_files", filename),None] for filename in ["1_Intro.pdf", "2_vision_board.pdf"]
-    ]
-    for file_type, folder_name in types.items():
-        all_filepaths = []
-        all_filepaths += intro_filepaths
-        ''' gears filepath'''
-        gears_filepath = os.path.join(current_directory, folder_name, "3_Gears and equipments.pdf")
-        all_filepaths += [[gears_filepath,None]]
-        ''' getting the monthly data'''
-        for imonth in range(1,13):
-            ''' cover path'''
-            month_cover_path = os.path.join(current_directory, 'all_files', "4_month_designs.pdf")
-            all_filepaths += [[month_cover_path,imonth-1]]
-            ''' getting the pdf of the months'''
-            daily_entries_folder = f'daily_entries_{version}_{file_type}'
-            all_files = os.listdir(os.path.join(current_directory,daily_entries_folder))
-            selected_files = [filename for filename in all_files if f'entry_{year}{str(imonth).zfill(2)}' in filename]
-            selected_files.sort()
-            daily_entry_paths = [[os.path.join(current_directory,daily_entries_folder,filename),None] for filename in selected_files]
-            all_filepaths += daily_entry_paths
-            ''' end of month '''
-            all_filepaths += [[os.path.join(current_directory,folder_name,'5_end_of_month.pdf'),None]]
-    
-        ''' merging the pdfs '''
-        filepaths = [sel_data[0] for sel_data in all_filepaths]
-        page_to_merge = [sel_data[1] for sel_data in all_filepaths]
-    
-        ''' merging the pdfs'''
-        output_filepath = os.path.join(current_directory,f"fitness_journal_{version}_{file_type}.pdf")
-        merge_pdfs(filepaths, output_filepath,page_to_merge=page_to_merge)
+def get_journal_merging_details():
+    details_intro = get_intro_merging_details()
+    details_monthly = get_monthly_merging_details()
+
+    merging_details = {}
+    for folder_type in foldernames:
+        merging_details.setdefault(folder_type, {})
+        merging_details[folder_type]["input_filepaths"] = (
+            details_intro[folder_type]["input_filepaths"]
+            + details_monthly[folder_type]["input_filepaths"]
+        )
+        merging_details[folder_type]["page_to_merge"] = (
+            details_intro[folder_type]["page_to_merge"]
+            + details_monthly[folder_type]["page_to_merge"]
+        )
+
+        """ output filepath"""
+        merging_details[folder_type]["output_filepath"] = os.path.join(
+            current_directory,
+            f"version_{input_dict['version']}",
+            folder_type,
+            f"journal_{input_dict['year']}.pdf",
+        )
+    return merging_details
+
+
+if __name__ == "__main__":
+    merging_details = get_journal_merging_details()
+#     ''' getting all the filepaths'''
+#     intro_filepaths = [
+#         [os.path.join(current_directory, "all_files", filename),None] for filename in ["1_Intro.pdf", "2_vision_board.pdf"]
+#     ]
+#     for file_type, folder_name in types.items():
+#         all_filepaths = []
+#         all_filepaths += intro_filepaths
+#         ''' gears filepath'''
+#         gears_filepath = os.path.join(current_directory, folder_name, "3_Gears and equipments.pdf")
+#         all_filepaths += [[gears_filepath,None]]
+#         ''' getting the monthly data'''
+#         for imonth in range(1,13):
+#             ''' cover path'''
+#             month_cover_path = os.path.join(current_directory, 'all_files', "4_month_designs.pdf")
+#             all_filepaths += [[month_cover_path,imonth-1]]
+#             ''' getting the pdf of the months'''
+#             daily_entries_folder = f'daily_entries_{version}_{file_type}'
+#             all_files = os.listdir(os.path.join(current_directory,daily_entries_folder))
+#             selected_files = [filename for filename in all_files if f'entry_{year}{str(imonth).zfill(2)}' in filename]
+#             selected_files.sort()
+#             daily_entry_paths = [[os.path.join(current_directory,daily_entries_folder,filename),None] for filename in selected_files]
+#             all_filepaths += daily_entry_paths
+#             ''' end of month '''
+#             all_filepaths += [[os.path.join(current_directory,folder_name,'5_end_of_month.pdf'),None]]
+
+#         ''' merging the pdfs '''
+#         filepaths = [sel_data[0] for sel_data in all_filepaths]
+#         page_to_merge = [sel_data[1] for sel_data in all_filepaths]
+
+#         ''' merging the pdfs'''
+#         output_filepath = os.path.join(current_directory,f"fitness_journal_{version}_{file_type}.pdf")
+#         merge_pdfs(filepaths, output_filepath,page_to_merge=page_to_merge)
